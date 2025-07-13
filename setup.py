a2654bac4db8ccd40cec034c7cd731779f5ed8dd
Juan Sanchez <juanpablosp.29@gmail.com>
2025-07-13 13:28:58 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repositorioReleases",                                             # Nombre del paquete
+    version="0.1.0",                                                        # Versión inicial
+    packages=find_packages(),                                               # Paquetes a incluir
+    description="Un paquete pip simple de saludo",                          # Breve descripción
+    author="Juan Pablo",                                                    # Tu nombre
+    author_email="juan-pablos29@hotmail.com",                               # Tu correo electrónico
+    url="https://github.com/juanpablos57/repositorioReleases",              # URL del proyecto
+)
